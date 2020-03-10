dbffac6b7b1ea772646a99302c7044ed80b17861
Grant Bruneel <grbruneel@gmail.com>
2020-03-10 10:26:47 -0600

    Full Screen Ready for the Pi

--- PiProject/homeScreen.py
+++ PiProject/homeScreen.py
@@ -13,14 +13,15 @@ window = tk.Tk()
 window.title("Cycles Home Screen")
 window.geometry("400x300")
 job = "0"
+is_fullscreen = True
 # This hides the cursor, Comment out when not using the touch screen
 # window.config(cursor="none")
 
 # Commands that go with Buttons
-def green():
+def start():
     global job
     if cycle_data.count >= cycle_data.max:
-        red()
+        stop()
         return
     out.off()
     out.rightOn()
@@ -29,13 +30,13 @@ def green():
     out.leftOn()
     cycle_data.increment()
     update_count()
-    job = window.after(cycle_data.retract_time, green)
+    job = window.after(cycle_data.retract_time, start)
 
 
 def update_count():
     cycle_count_number.config(text=cycle_data.count)
 
-def red():
+def stop():
     global job
     out.off()
     window.after_cancel(job)
@@ -43,24 +44,38 @@ def red():
 
 def reset_settings():
     global cycle_data
-    red()
+    stop()
     reset_data.show(cycle_data)
     cycle_limit_number.config(text=cycle_data.max)
     cycle_count_number.config(text=cycle_data.count)
 
 def time_action():
     global cycle_data
-    red()
+    stop()
     time_data.show()
     # Not currently shown on main Screen
 
+def close_fullscreen(Event):
+    global is_fullscreen
+    is_fullscreen = False
+    print(is_fullscreen)
+    window.attributes("-fullscreen", is_fullscreen)
+
+def toggle_fullscreen(Event):
+    global is_fullscreen
+    if is_fullscreen:
+        is_fullscreen = False
+    else:
+        is_fullscreen = True
+    window.attributes("-fullscreen", is_fullscreen)
+
 
 # Buttons on the Home Screen
-start_Button = tk.Button(window, text="START", bg="Green", command=green)
+start_Button = tk.Button(window, text="START", bg="Green", command=start)
 start_Button.grid(row=0, column=0)
 start_Button.config(height=5, width=15)
 
-stop_Button = tk.Button(window, text="STOP", bg="Red", command=red)
+stop_Button = tk.Button(window, text="STOP", bg="Red", command=stop)
 stop_Button.grid(row=0, column=2)
 stop_Button.config(height=5, width=15)
 
@@ -83,5 +98,9 @@ cycle_limit_text.grid(row=2, column=0)
 cycle_limit_number = tk.Label(window, text=cycle_data.max)
 cycle_limit_number.grid(row=2, column=1)
 
+# Full Screen
+window.bind("<Escape>", close_fullscreen)
+window.bind("<F11>", toggle_fullscreen)
+window.attributes("-fullscreen", is_fullscreen)
 
 window.mainloop()
